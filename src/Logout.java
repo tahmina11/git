b542ae4f47f78ad3116f21df5a9f9e883916baf8
Tahmina <Tahmina@Tahminas-MacBook-Air.local>
2020-02-29 11:34:06 -0500

    updated Logout.java

--- src/Logout.java
+++ src/Logout.java
@@ -1,5 +1,8 @@
 public class Logout {
     public static void main(String[] args) {
-        System.out.println("java is fun");
+        System.out.println("java fun");
+        //updated ne code
+// blue mean it is updated, some feature are deleted
+        //this green column mean it is new , and not in local resectory
     }
 }
